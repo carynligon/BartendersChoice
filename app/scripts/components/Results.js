b58c4c91534c17cc9b1afed222fa408e7ec35483
Caryn Ligon <carynligon@gmail.com>
2016-08-15 22:29:29 -0500

    still working on matching function, still unsuccessful

--- app/scripts/components/Results.js
+++ app/scripts/components/Results.js
@@ -11,6 +11,38 @@ export default React.createClass({
     };
   },
   compareScores() {
+    let matches = [];
+    this.state.results.personality.personality_types.forEach((trait) => {
+      let traitName = trait.personality_type.name;
+      console.log(traitName);
+      let score = trait.score;
+      console.log(score);
+      this.state.drinks.forEach((drink) => {
+        if (Math.abs(drink[traitName] - score) <= 10) {
+          matches.push(drink);
+        }
+      });
+      console.log(matches);
+      let counter = 1;
+      function sliceMatch(drink) {
+        let index = matches.indexOf(drink);
+        matches.slice(index, 1);
+        counter ++;
+      }
+      matches.forEach((drink) => {
+        sliceMatch(drink);
+        if (counter === 7) {
+          console.log(drink);
+        }
+        if (matches.indexOf(drink) !== -1) {
+          sliceMatch(drink);
+        }
+      });
+      // let reducedMatch = matches.filter((drink) => {
+      //   if (prev.drink_strDrink === curr.drink_strDrink)
+      // };
+      // console.log(reducedMatch);
+    });
   },
   listener() {
     this.setState({
@@ -29,14 +61,9 @@ export default React.createClass({
     store.slides.sendAnswers(store.session.get('assessment_id'));
     console.log(this.state);
     let traits;
-    let testScores = [];
-    this.compareScores();
     if (this.state.results.personality) {
-      let flatArr = _.flatten(this.state.results.personality.personality_types);
-      console.log(flatArr);
+      this.compareScores();
       traits = this.state.results.personality.personality_types.map((type, i) => {
-        testScores.push({trait: type.personality_type.name, score: type.score});
-        console.log(testScores);
         return (
           <li key={i}>
             <h5>{type.personality_type.name}</h5>
