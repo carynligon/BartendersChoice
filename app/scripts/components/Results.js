ed921dc7ac1ac89af783e562ef5fe1cebf4861fc
Caryn Ligon <carynligon@gmail.com>
2016-08-16 12:10:49 -0500

    rendering properly

--- app/scripts/components/Results.js
+++ app/scripts/components/Results.js
@@ -1,5 +1,6 @@
 import _ from 'underscore';
 import React from 'react';
+import {Link} from 'react-router';
 
 import store from '../store';
 
@@ -12,7 +13,10 @@ export default React.createClass({
   },
   compareScores() {
     let matches = [];
-
+    this.setState({
+      results: store.session.toJSON(),
+      drinks: store.cocktails.toJSON()
+    })
     this.state.results.personality.personality_types.forEach((trait, i) => {
       let traitName = trait.personality_type.name;
       console.log(traitName);
@@ -27,10 +31,8 @@ export default React.createClass({
         // console.log(obj);
         if (obj) {
           if (obj[curr]) {
-            console.log('if ran');
             obj[curr] = obj[curr] + 1;
           } else {
-            console.log('else ran');
             obj[curr] = 1;
           }
         }
@@ -40,45 +42,56 @@ export default React.createClass({
         return drink[1] === _.max(reducedMatch);
       });
       if (i ===6) {
-        console.log(fullMatches);
         let index = Math.floor(Math.random() * fullMatches.length);
         let drinkId = fullMatches[index][0];
         let drink = store.cocktails.get(drinkId);
-        console.log(drink);
+        this.setState({
+          matchedDrink: drink.toJSON(),
+        });
       }
     });
   },
-  listener() {
-    this.setState({
-      results: store.session.toJSON(),
-      drinks: store.cocktails.toJSON()
-    });
-  },
+  // listener() {
+  //   this.setState({
+  //     results: store.session.toJSON(),
+  //     drinks: store.cocktails.toJSON()
+  //   });
+  // },
   componentDidMount() {
-    store.session.on('change update', this.listener);
+    store.session.on('change update', this.compareScores);
     store.cocktails.fetch();
   },
   componentWillUnmount() {
-    store.session.off('change update', this.listener);
+    store.session.off('change update', this.compareScores);
   },
   render() {
     store.slides.sendAnswers(store.session.get('assessment_id'));
     console.log(this.state);
     let traits;
+    let drinkName;
+    let drinkId;
+    let drinkImg;
     if (this.state.results.personality) {
-      this.compareScores();
-      traits = this.state.results.personality.personality_types.map((type, i) => {
-        return (
-          <li key={i}>
-            <h5>{type.personality_type.name}</h5>
-            <p>{type.personality_type.description}</p>
-            <p>{type.score}</p>
-          </li>
-        );
-      });
+      if (this.state.matchedDrink) {
+        drinkName = this.state.matchedDrink.drink__strDrink;
+        drinkId = this.state.matchedDrink._id;
+        drinkImg = this.state.matchedDrink.drink__strDrinkThumb;
+        traits = this.state.results.personality.personality_types.map((type, i) => {
+          return (
+            <li key={i}>
+              <h5>{type.personality_type.name}</h5>
+              <p>{type.personality_type.description}</p>
+              <p>{type.score}</p>
+            </li>
+          );
+        });
+      }
     }
     return (
       <section id="results-section">
+        <h3>Your Drink: {drinkName}</h3>
+        <img src={drinkImg}/>
+        <Link to={`recipe/${drinkId}`}>Recipe</Link>
         <ul>
           {traits}
         </ul>
