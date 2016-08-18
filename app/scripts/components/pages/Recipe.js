ee08c147c7fff99a63ff219efacf7d6c7271375c
Caryn Ligon <carynligon@gmail.com>
2016-08-17 22:22:10 -0500

    started formatting recipe page

--- app/scripts/components/pages/Recipe.js
+++ app/scripts/components/pages/Recipe.js
@@ -21,24 +21,34 @@ export default React.createClass({
     store.cocktails.off('update', this.listener);
   },
   render() {
+    console.log(this.state);
+    let background = {
+      background: 'url(' + this.state.drink__strDrinkThumb
+ + ')'
+    }
     return(
       <main>
         <Nav/>
         <input type="button" id="back-btn" value="back" onClick={this.sendBack}/>
-        <h2>{this.state.drink__strDrink}</h2>
-        <h4>Ingredients</h4>
-        <ul>
-          <li>{this.state.drink__strIngredient1} {this.state.drink__strMeasure1}</li>
-          <li>{this.state.drink__strIngredient2} {this.state.drink__strMeasure2}</li>
-          <li>{this.state.drink__strIngredient3} {this.state.drink__strMeasure3}</li>
-          <li>{this.state.drink__strIngredient4} {this.state.drink__strMeasure4}</li>
-          <li>{this.state.drink__strIngredient5} {this.state.drink__strMeasure5}</li>
-          <li>{this.state.drink__strIngredient6} {this.state.drink__strMeasure6}</li>
-          <li>{this.state.drink__strIngredient7} {this.state.drink__strMeasure7}</li>
-          <li>{this.state.drink__strIngredient8} {this.state.drink__strMeasure8}</li>
-        </ul>
-        <h4>Mixing Instructions</h4>
-        <p>{this.state.drink__strInstructions}</p>
+        <h2 className="recipe-title">{this.state.drink__strDrink}</h2>
+        <div className="ingredients">
+          <h4>Ingredients</h4>
+          <ul className="ingredients-list">
+            <li>{this.state.drink__strIngredient1} {this.state.drink__strMeasure1}</li>
+            <li>{this.state.drink__strIngredient2} {this.state.drink__strMeasure2}</li>
+            <li>{this.state.drink__strIngredient3} {this.state.drink__strMeasure3}</li>
+            <li>{this.state.drink__strIngredient4} {this.state.drink__strMeasure4}</li>
+            <li>{this.state.drink__strIngredient5} {this.state.drink__strMeasure5}</li>
+            <li>{this.state.drink__strIngredient6} {this.state.drink__strMeasure6}</li>
+            <li>{this.state.drink__strIngredient7} {this.state.drink__strMeasure7}</li>
+            <li>{this.state.drink__strIngredient8} {this.state.drink__strMeasure8}</li>
+          </ul>
+        </div>
+        <div id="recipe-drink-img" style={background}></div>
+        <div className="mixing-instructions">
+          <h4>Mixing Instructions</h4>
+          <p>{this.state.drink__strInstructions}</p>
+        </div>
       </main>
     );
   }
