ab2c39a3667a02aa2facb8d016e1f0b6c6bdd401
Caryn Ligon <carynligon@gmail.com>
2016-08-18 12:08:54 -0500

    got drinkIngredient data up, saving function

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import ReactDOM from 'react-dom';
 import {hashHistory} from 'react-router';
+import _ from 'underscore';
 
 import router from './router';
 import store from './store';
@@ -14,42 +15,60 @@ $(document).ajaxSend(function(evt, xhrAjax, jqueryAjax) {
     }
 });
 
+
 $.ajax({
   url: `https://baas.kinvey.com/appdata/${settings.appKey}/Cocktails`,
   success: (data) => {
-    console.log(data);
-    let allIngredients = [];
-    data.map((drink) => {
-      allIngredients.push(drink.drink__strIngredient1);
-      allIngredients.push(drink.drink__strIngredient2);
-      allIngredients.push(drink.drink__strIngredient3);
-      allIngredients.push(drink.drink__strIngredient4);
-      allIngredients.push(drink.drink__strIngredient5);
-      allIngredients.push(drink.drink__strIngredient6);
-      allIngredients.push(drink.drink__strIngredient7);
-      allIngredients.push(drink.drink__strIngredient8);
-      allIngredients.push(drink.drink__strIngredient9);
-    });
-    let unique = [];
-    let reduced = allIngredients.reduce((retsf, curr) => {
-      if (retsf.indexOf(curr) === -1 && curr !== null) {
-        retsf.push(curr);
+    let cocktails = data;
+    $.ajax({
+      url: `https://baas.kinvey.com/appdata/${settings.appKey}/Ingredients`,
+      success: (data) => {
+        let ingredients = data;
+        let drinkIngredients = [];
+        cocktails.forEach((drink) => {
+          let ingredientArr = [
+            {drinkId: drink._id, ingredient: drink.drink__strIngredient1, quantity: drink.drink__strMeasure1},
+            {drinkId: drink._id, ingredient: drink.drink__strIngredient2, quantity: drink.drink__strMeasure2},
+            {drinkId: drink._id, ingredient: drink.drink__strIngredient3, quantity: drink.drink__strMeasure3},
+            {drinkId: drink._id, ingredient: drink.drink__strIngredient4, quantity: drink.drink__strMeasure4},
+            {drinkId: drink._id, ingredient: drink.drink__strIngredient5, quantity: drink.drink__strMeasure5},
+            {drinkId: drink._id, ingredient: drink.drink__strIngredient6, quantity: drink.drink__strMeasure6},
+            {drinkId: drink._id, ingredient: drink.drink__strIngredient7, quantity: drink.drink__strMeasure7},
+            {drinkId: drink._id, ingredient: drink.drink__strIngredient8, quantity: drink.drink__strMeasure8},
+            {drinkId: drink._id, ingredient: drink.drink__strIngredient9, quantity: drink.drink__strMeasure9},
+          ]
+          let nullRemoved = [];
+          ingredientArr.forEach((ingredient) => {
+            if (ingredient.ingredient !== null) {
+              nullRemoved.push(ingredient);
+            }
+          });
+          drinkIngredients.push(nullRemoved);
+        });
+        (_.flatten(drinkIngredients)).forEach((drinkIngredient) => {
+          ingredients.forEach((ingredient) => {
+            if (ingredient.ingredient === drinkIngredient.ingredient) {
+              drinkIngredient.ingredientID = ingredient._id;
+            }
+          });
+        });
+        (_.flatten(drinkIngredients)).forEach((ingredient) => {
+          $.ajax({
+            url: `https://baas.kinvey.com/appdata/${settings.appKey}/drinkIngredients`,
+            type: 'POST',
+            data: {
+              ingredient: ingredient.ingredient,
+              ingredientID: ingredient.ingredientID,
+              drinkID: ingredient.drinkId,
+              quantity: ingredient.quantity
+            }
+          });
+        });
       }
-      return retsf;
-    },[]);
-    console.log(reduced);
-    reduced.forEach((ingredient) => {
-      $.ajax({
-        url: `https://baas.kinvey.com/appdata/${settings.appKey}/Ingredients`,
-        type: 'POST',
-        data: {ingredient: ingredient},
-        success: (data) => {
-          console.log(data);
-        }
-      });
     });
   }
 });
+
 
 // ReactDOM.render(router, document.getElementById('container'));
 //
