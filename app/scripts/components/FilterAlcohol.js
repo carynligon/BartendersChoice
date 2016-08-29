94138d64c1b59acec4f9136254f9a59128cd0c95
Caryn Ligon <carynligon@gmail.com>
2016-08-29 15:34:47 -0500

    all pages are protected by confirm page and custom page reverts to home if user is not signed in

--- app/scripts/components/FilterAlcohol.js
+++ app/scripts/components/FilterAlcohol.js
@@ -2,55 +2,33 @@ import React from 'react';
 import {hashHistory} from 'react-router';
 
 export default React.createClass({
+  getInitialState() {
+    return {
+      selected: []
+    };
+  },
   filterAlcohol(e) {
-    // let alcohol = e.target.id;
-    // if (document.getElementById('search-input').value !== '') {
-    //   hashHistory.push({
-    //     pathname: 'search',
-    //     query: {
-    //       q: document.getElementById('search-input').value,
-    //       filter: [alcohol]
-    //     }
-    //   });
-    // } else {
-    //   hashHistory.push({
-    //     pathname: 'search',
-    //     query: {filter: [alcohol]}
-    //   });
-    // }
+    console.dir(e.target)
+    this.setState({selected: this.state.selected.push(e.target.id)});
   },
   render() {
     return (
       <div id="filter-alcohol-wrapper">
         <h6>Alcohol</h6>
-        <ul id="alcohol-list-1">
-          <li>
+        <form id="alcohol-list-1">
             <input type="radio" id="bourbon" onChange={this.filterAlcohol}/>
             <label htmlFor="bourbon">Bourbon</label>
-          </li>
-          <li>
             <input type="radio" id="rye" onChange={this.filterAlcohol}/>
             <label htmlFor="rye">Rye</label>
-          </li>
-          <li>
             <input type="radio" id="tequila" onChange={this.filterAlcohol}/>
             <label htmlFor="tequila">Tequila</label>
-          </li>
-        </ul>
-        <ul id="alcohol-list-2">
-          <li>
             <input type="radio" id="gin" onChange={this.filterAlcohol}/>
             <label htmlFor="gin">Gin</label>
-          </li>
-          <li>
             <input type="radio" id="vodka" onChange={this.filterAlcohol}/>
             <label htmlFor="vodka">Vodka</label>
-          </li>
-          <li>
             <input type="radio" id="scotch" onChange={this.filterAlcohol}/>
             <label htmlFor="scotch">Scotch</label>
-          </li>
-        </ul>
+        </form>
       </div>
     );
   }
--- app/scripts/router.js
+++ app/scripts/router.js
@@ -15,21 +15,35 @@ import Results from './components/pages/Results';
 import Search from './components/pages/Search';
 import Custom from './components/pages/Custom';
 
+function requireAuth(nextState, replace) {
+  if (!localStorage.getItem('ofAge')) {
+    replace({
+      pathname: '/confirm',
+      state: {nextPathname: nextState.location.pathname}
+    });
+  } else if (this.path === '/custom' && localStorage.getItem('username') === 'Anonymous') {
+    replace({
+      pathname: '/',
+      state: {nextPathname: nextState.location.pathname}
+    })
+  }
+}
+
 const router = (
   <Router history={hashHistory}>
     <Route path="/confirm" component={Confirm}/>
-    <Route path="/" component={App}>
+    <Route path="/" component={App} onEnter={requireAuth}>
     </Route>
-    <Route path="/search" component={Search}/>
-    <Route path="/recipe/:cocktail" component={Recipe}/>
-    <Route path="/assessment" component={Assessment}>
+    <Route path="/search" component={Search} onEnter={requireAuth}/>
+    <Route path="/recipe/:cocktail" component={Recipe} onEnter={requireAuth}/>
+    <Route path="/assessment" component={Assessment} onEnter={requireAuth}>
       <IndexRoute component={AssessmentInfo}/>
       <Route path="/assessment/question/:id" component={SingleQuestion}/>
       <Route path="/assessment/results" component={Results}/>
     </Route>
-    <Route path="/custom" component={Custom}/>
-    <Route path="/me" component={UserDashboard}/>
-    <Route path="*" component={NotFound}/>
+    <Route path="/custom" component={Custom} onEnter={requireAuth}/>
+    <Route path="/me" component={UserDashboard} onEnter={requireAuth}/>
+    <Route path="*" component={NotFound} onEnter={requireAuth}/>
   </Router>
 );
 
