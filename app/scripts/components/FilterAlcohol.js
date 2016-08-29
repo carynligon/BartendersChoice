ce5762299265bf3618913483c67a0f110fa0ba84
Caryn Ligon <carynligon@gmail.com>
2016-08-29 15:38:38 -0500

    made alcohol radio buttons belong to same group so they are exclusive

--- app/scripts/components/FilterAlcohol.js
+++ app/scripts/components/FilterAlcohol.js
@@ -2,31 +2,22 @@ import React from 'react';
 import {hashHistory} from 'react-router';
 
 export default React.createClass({
-  getInitialState() {
-    return {
-      selected: []
-    };
-  },
-  filterAlcohol(e) {
-    console.dir(e.target)
-    this.setState({selected: this.state.selected.push(e.target.id)});
-  },
   render() {
     return (
       <div id="filter-alcohol-wrapper">
         <h6>Alcohol</h6>
         <form id="alcohol-list-1">
-            <input type="radio" id="bourbon" onChange={this.filterAlcohol}/>
+            <input type="radio" name="alcohol" id="bourbon" onChange={this.filterAlcohol}/>
             <label htmlFor="bourbon">Bourbon</label>
-            <input type="radio" id="rye" onChange={this.filterAlcohol}/>
+            <input type="radio" name="alcohol" id="rye" onChange={this.filterAlcohol}/>
             <label htmlFor="rye">Rye</label>
-            <input type="radio" id="tequila" onChange={this.filterAlcohol}/>
+            <input type="radio" name="alcohol" id="tequila" onChange={this.filterAlcohol}/>
             <label htmlFor="tequila">Tequila</label>
-            <input type="radio" id="gin" onChange={this.filterAlcohol}/>
+            <input type="radio" name="alcohol" id="gin" onChange={this.filterAlcohol}/>
             <label htmlFor="gin">Gin</label>
-            <input type="radio" id="vodka" onChange={this.filterAlcohol}/>
+            <input type="radio" name="alcohol" id="vodka" onChange={this.filterAlcohol}/>
             <label htmlFor="vodka">Vodka</label>
-            <input type="radio" id="scotch" onChange={this.filterAlcohol}/>
+            <input type="radio" name="alcohol" id="scotch" onChange={this.filterAlcohol}/>
             <label htmlFor="scotch">Scotch</label>
         </form>
       </div>
